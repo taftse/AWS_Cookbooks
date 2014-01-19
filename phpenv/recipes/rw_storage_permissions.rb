832cb4e5869171bc088011dd26476daa4bea597e
Timothy Seebus <Timothyseebus@tools4schools.org>
2014-01-19 01:48:18 +0000

    added some updated opsworks cookbooks

--- phpenv/recipes/rw_storage_permissions.rb
+++ phpenv/recipes/rw_storage_permissions.rb
@@ -1,5 +1,6 @@
-node[:deploy].each do |application, deploy|
-  app_root = "#{deploy[:deploy_to]}/current/app/storage"
-  execute "chmod -R g+rw #{app_root}" do
-  end
+directory "#{deploy[:deploy_to]}/current/app/storage" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
 end
