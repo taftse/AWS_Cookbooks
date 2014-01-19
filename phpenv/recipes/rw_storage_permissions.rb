72905feba1745d32f0f72cf070a26bd812505757
Timothy Seebus <Timothyseebus@tools4schools.org>
2014-01-19 23:04:39 +0000

    fix ?

--- phpenv/recipes/rw_storage_permissions.rb
+++ phpenv/recipes/rw_storage_permissions.rb
@@ -1,6 +1,8 @@
-directory "#{deploy[:deploy_to]}/current/app/storage" do
-  owner "root"
-  group "root"
-  mode 0755
-  action :create
+node[:deploy].each do |application, deploy|
+	directory "#{deploy[:deploy_to]}/current/app/storage" do
+	  owner "root"
+	  group "root"
+	  mode 0755
+	  action :create
+	end
 end
