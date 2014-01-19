2c8c4d93bff856dcdff1ba1931adbe936fb088fa
Timothy Seebus <Timothyseebus@tools4schools.org>
2014-01-19 02:54:57 +0000

    update php dependancies

--- mod_php5_apache2/specs/default_spec.rb
+++ mod_php5_apache2/specs/default_spec.rb
@@ -29,21 +29,21 @@ describe_recipe 'mod_php5_apache2::default' do
 
     when 'redhat', 'centos', 'fedora', 'amazon'
       packages = [
-        'php-xml',
-        'php-common',
-        'php-xmlrpc',
-        'php-devel',
-        'php-gd',
-        'php-cli',
-        'php-pear-Auth-SASL',
-        'php-mysql',
-        'php-mcrypt',
-        'php-pecl-memcache',
-        'php-pear',
-        'php-pear-XML-Parser',
-        'php-pear-Mail-Mime',
-        'php-pear-DB',
-       'php-pear-HTML-Common'
+        'php55-xml',
+        'php55-common',
+        'php55-xmlrpc',
+        'php55-devel',
+        'php55-gd',
+        'php55-cli',
+        'php55-pear-Auth-SASL',
+        'php55-mysql',
+        'php55-mcrypt',
+        'php55-pecl-memcache',
+        'php55-pear',
+        'php55-pear-XML-Parser',
+        'php55-pear-Mail-Mime',
+        'php55-pear-DB',
+       'php55-pear-HTML-Common'
       ]
     end
 
