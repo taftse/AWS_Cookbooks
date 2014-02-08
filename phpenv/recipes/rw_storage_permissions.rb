44c04c53039ccd9fd9889485561d90b0d43d3d6a
Timothy Seebus <Timothyseebus@tools4schools.org>
2014-02-08 17:09:07 +0000

    update permissions

--- phpenv/recipes/rw_storage_permissions.rb
+++ phpenv/recipes/rw_storage_permissions.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |application, deploy|
 	directory "#{deploy[:deploy_to]}/current/app/storage" do
-	  owner "root"
-	  group "root"
+	  owner "deploy"
+	  group "apache"
 	  mode 0777
 	  action :create
 	end
